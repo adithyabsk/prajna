5ab9eca7a2cb0c409c65937ec3eed78b0ef36c23
Adithya Balaji <adithyabsk@gmail.com>
2021-12-10 19:47:45 -0500

    Switch from Pedersen Hash function to MiMC Sponge Hash
    * This is more suited for smart contract environments
      * Bonus: has much fewer dependencies
    * Switch from Node14 to Node15 and update package-lock.json

--- circuits/main.circom
+++ circuits/main.circom
@@ -1,5 +1,5 @@
 pragma circom 2.0.0;
 
-include "./circuit_lib/pedersen.circom";
+include "./circuit_lib/mimcsponge.circom";
 
-component main = Pedersen(256);
+component main = MiMCSponge(1, 220, 3);
--- test/test-circuit.js
+++ test/test-circuit.js
@@ -1,6 +1,8 @@
 const { describe } = require("mocha");
-const path = require("path");
 const fs = require('fs');
+
+const path = require("path");
+const buildMimcSponge = require("circomlibjs").buildMimcSponge;
 
 const buildPedersenHash = require("circomlibjs").buildPedersenHash;
 const buildBabyJub = require("circomlibjs").buildBabyjub; // field to point
@@ -12,6 +14,43 @@ const {
     strToBitArray,
     bitArray2buffer
 } = require("../src/index.js");
+
+
+describe("MiMC Sponge Circuit test", function () {
+    let circuit;
+    let mimcSponge;
+    let F;
+
+    this.timeout(100000);
+
+    before( async () => {
+        mimcSponge = await buildMimcSponge();
+        F = mimcSponge.F;
+    });
+    // after(async () => {
+    //     globalThis.curve_bn128.terminate();
+    // });
+
+    it("Should check hash", async () => {
+        const ckt_path = path.join(__dirname, "..", "circuits")
+        circuit = await wasm_tester(path.join(ckt_path, "main.circom"));
+
+        const input = {ins: [1337], k: 0}
+        const w = await circuit.calculateWitness(input);
+
+        fs.writeFile(path.join(ckt_path, "hash_input.json"), JSON.stringify(input), function(err) {
+            if (err) console.log(err);
+        });
+
+        const out2 = mimcSponge.multiHash([1337], 0, 3);
+
+        for (let i=0; i<out2.length; i++) out2[i] = F.toObject(out2[i]);
+
+        await circuit.assertOut(w, {outs: out2});
+
+        await circuit.checkConstraints(w);
+    });
+});
 
 describe("Pedersen test", function() {
     let babyJub
